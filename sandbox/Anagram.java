ac522578f4b92533700f3c7b2ed03aef9dc91574
deer_13D68B06D5 <deer@cyber-dojo.org>
2015-12-11 22:43:53 +0000

    2

--- sandbox/Anagram.java
+++ sandbox/Anagram.java
@@ -11,6 +11,8 @@ public class Anagram {
             return Arrays.asList(string.substring(0, 1) + string.substring(1), 
                                  string.substring(1) + string.substring(0, 1));
         }
-        return Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba");
+        return Arrays.asList("abc", "acb", 
+                             "bac", "bca", 
+                             "cab", "cba");
     }
 }
