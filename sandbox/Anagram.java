ee47fcc403e2474b93fd14db3cb3db73d76c8819
deer_13D68B06D5 <deer@cyber-dojo.org>
2015-12-11 22:53:43 +0000

    6

--- sandbox/Anagram.java
+++ sandbox/Anagram.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Anagram {
     public List<String> generate(String string) {
         if (string.length() == 1) {
-            return Arrays.asList(string.substring(0, 1));
+            return Arrays.asList(string);
         }
         if (string.length() == 2) {
             return Arrays.asList(string, 
