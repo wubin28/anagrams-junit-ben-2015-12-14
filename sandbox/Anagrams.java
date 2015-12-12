649740c61756723914ff8f72d6c61a4a68942a94
deer_13D68B06D5 <deer@cyber-dojo.org>
2015-12-12 00:03:34 +0000

    24

--- sandbox/Anagrams.java
+++ sandbox/Anagrams.java
@@ -11,11 +11,12 @@ public class Anagrams {
             return Arrays.asList(s, 
                                  s.substring(1) + s.substring(0, 1));
         }
-        return Arrays.asList(s.substring(0, 1) + Anagrams.of(dropCharacter(s, 0).get(0), 
+        return Arrays.asList(s.substring(0, 1) + Anagrams.of(dropCharacter(s, 0)).get(0), 
                              s.substring(0, 1) + Anagrams.of(dropCharacter(s, 0)).get(1), 
                              s.substring(1, 2) + Anagrams.of("ac").get(0), 
                              s.substring(1, 2) + Anagrams.of("ac").get(1), 
                              s.substring(2) + Anagrams.of("ab").get(0), 
-                             s.substring(2) + Anagrams.of("ab").get(1));
+                             s.substring(2) + Anagrams.of("ab").get(1)
+                             );
     }
 }
