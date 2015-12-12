5f08c1e1765b738f45291cf55eaf08a1b8ab1c97
deer_13D68B06D5 <deer@cyber-dojo.org>
2015-12-12 00:02:45 +0000

    23

--- sandbox/Anagrams.java
+++ sandbox/Anagrams.java
@@ -3,19 +3,19 @@ import java.util.List;
 import java.util.Arrays;
 
 public class Anagrams {
-    public static List<String> of(String string) {
-        if (string.length() == 1) {
-            return Arrays.asList(string);
+    public static List<String> of(String s) {
+        if (s.length() == 1) {
+            return Arrays.asList(s);
         }
-        if (string.length() == 2) {
-            return Arrays.asList(string, 
-                                 string.substring(1) + string.substring(0, 1));
+        if (s.length() == 2) {
+            return Arrays.asList(s, 
+                                 s.substring(1) + s.substring(0, 1));
         }
-        return Arrays.asList(string.substring(0, 1) + Anagrams.of("bc").get(0), 
-                             string.substring(0, 1) + Anagrams.of("bc").get(1), 
-                             string.substring(1, 2) + Anagrams.of("ac").get(0), 
-                             string.substring(1, 2) + Anagrams.of("ac").get(1), 
-                             string.substring(2) + Anagrams.of("ab").get(0), 
-                             string.substring(2) + Anagrams.of("ab").get(1));
+        return Arrays.asList(s.substring(0, 1) + Anagrams.of(dropCharacter(s, 0).get(0), 
+                             s.substring(0, 1) + Anagrams.of(dropCharacter(s, 0)).get(1), 
+                             s.substring(1, 2) + Anagrams.of("ac").get(0), 
+                             s.substring(1, 2) + Anagrams.of("ac").get(1), 
+                             s.substring(2) + Anagrams.of("ab").get(0), 
+                             s.substring(2) + Anagrams.of("ab").get(1));
     }
 }
