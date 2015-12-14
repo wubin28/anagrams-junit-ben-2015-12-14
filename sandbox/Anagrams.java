6aaf22baf22f2ae5ea705988ca2eb2b35c7695e6
deer_13D68B06D5 <deer@cyber-dojo.org>
2015-12-14 11:13:54 +0000

    39

--- sandbox/Anagrams.java
+++ sandbox/Anagrams.java
@@ -11,13 +11,14 @@ public class Anagrams {
             return Arrays.asList(s, 
                                  s.substring(1) + s.substring(0, 1));
         }
-        return Arrays.asList(s.substring(0, 1) + Anagrams.of(dropCharacter(s, 0)).get(0), 
-                             s.substring(0, 1) + Anagrams.of(dropCharacter(s, 0)).get(1), 
-                             s.substring(1, 2) + Anagrams.of(dropCharacter(s, 1)).get(0), 
-                             s.substring(1, 2) + Anagrams.of(dropCharacter(s, 1)).get(1), 
-                             s.substring(2) + Anagrams.of(dropCharacter(s, 2)).get(0), 
-                             s.substring(2) + Anagrams.of(dropCharacter(s, 2)).get(1)
-                             );
+        List<String> anagrams = new ArrayList<String>();
+        anagrams.add(s.substring(0, 1) + Anagrams.of(dropCharacter(s, 0)).get(0));
+        anagrams.add(s.substring(0, 1) + Anagrams.of(dropCharacter(s, 0)).get(1));
+        anagrams.add(s.substring(1, 2) + Anagrams.of(dropCharacter(s, 1)).get(0));
+        anagrams.add(s.substring(1, 2) + Anagrams.of(dropCharacter(s, 1)).get(1));
+        anagrams.add(s.substring(2) + Anagrams.of(dropCharacter(s, 2)).get(0));
+        anagrams.add(s.substring(2) + Anagrams.of(dropCharacter(s, 2)).get(1));
+        return anagrams;
     }
 
     private static String dropCharacter(String s, int index) {
